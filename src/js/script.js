e217690a517b0162ff6f13b02de581efef32792a
Krystian Błasiak <blasiakkrystian@o2.pl>
2026-02-18 13:16:16 +0100

    Add method initAccordion to class Product in file script.js

--- src/js/script.js
+++ src/js/script.js
@@ -61,6 +61,7 @@ const select = {
       thisProduct.data = data;
 
       thisProduct.renderInMenu();
+      thisProduct.initAccordion();
       console.log("New Product", thisProduct);
     }
 
@@ -78,6 +79,36 @@ const select = {
 
       /* add element to menu */
       menuContainer.appendChild(thisProduct.element);
+
+    }
+
+    initAccordion(){
+      const thisProduct = this;
+
+      /* find the clickable trigger (the element that should react to clicking) */
+      const clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
+      console.log("clickableTrigger: ", clickableTrigger);
+      
+      /* START: add event listener to clickable trigger on event click */
+      clickableTrigger.addEventListener("click", function(event){
+        /* prevent default action for event */
+        event.preventDefault();
+        console.log("thisProduct: ", thisProduct.element);
+
+        /* find active product (product that has active class) */
+        const activeProduct = document.querySelector(".product.active");
+        console.log("activeProduct: ", activeProduct);
+
+        /* if there is active product and it's not thisProduct.element, remove class active from it */
+        if(activeProduct != null && activeProduct != thisProduct.element){
+          activeProduct.classList.remove("active");
+        }
+
+        /* toggle active class on thisProduct.element */
+        thisProduct.element.classList.toggle("active");
+        
+
+      });
 
     }
   }
