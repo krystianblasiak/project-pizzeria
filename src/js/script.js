b23b01605204e519d37e782250f767b90e15d076
Krystian Błasiak <blasiakkrystian@o2.pl>
2026-02-19 11:11:44 +0100

    Add method processOrder to class Product in file script.js

--- src/js/script.js
+++ src/js/script.js
@@ -63,6 +63,8 @@ const select = {
       thisProduct.renderInMenu();
       thisProduct.getElements();
       thisProduct.initAccordion();
+      thisProduct.initOrderForm();
+      thisProduct.processOrder();
       console.log("New Product", thisProduct);
     }
 
@@ -118,9 +120,72 @@ const select = {
         /* toggle active class on thisProduct.element */
         thisProduct.element.classList.toggle("active");
         
+      });
+    }
 
+    initOrderForm(){
+      const thisProduct = this;
+
+      thisProduct.form.addEventListener("submit", function(event){
+        event.preventDefault();
+        thisProduct.processOrder();
       });
 
+      for(let input of thisProduct.formInputs){
+        input.addEventListener("change", function(){
+          thisProduct.processOrder();
+        });
+      }
+
+      thisProduct.cartButton.addEventListener("click", function(event){
+        event.preventDefault();
+        thisProduct.processOrder();
+      });
+
+      console.log("initOrderForm");
+    }
+
+    processOrder(){
+      const thisProduct = this;
+
+      // covert form to object structure
+      const formData = utils.serializeFormToObject(thisProduct.form);
+      console.log("formData", formData);
+
+      // set price to default price
+      let price = thisProduct.data.price;
+
+      // for every category (param)...
+      for(let paramId in thisProduct.data.params){
+        // determine param value
+        const param = thisProduct.data.params[paramId];
+        console.log(paramId, param);
+
+        // for every option in this category
+        for(let optionId in param.options){
+          // determine option value
+          const option = param.options[optionId];
+          console.log(optionId, option);
+
+          // check if there is param with a name of paramId in formData and if it includes optionId
+          if(formData[paramId] && formData[paramId].includes(optionId)) {
+            // check if the option is not default
+            if(!option.default) {
+              // add option price to price variable
+              price += option.price;
+            }
+          } else {
+            // check if the option is default
+            if(option.default) {
+              // reduce price variable
+              price -= option.price;
+            }
+          }
+        }
+      }
+      console.log("price: ", price);
+      // update calculated price in the HTML
+      thisProduct.priceElem.innerHTML = price;
     }
   }
 
