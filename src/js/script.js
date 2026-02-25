b37e19d4b0faca6fbe28fc0d91ce897de357cc99
Krystian Błasiak <blasiakkrystian@o2.pl>
2026-02-25 19:27:30 +0100

    Add method initActions to class Cart in file script.js

--- src/js/script.js
+++ src/js/script.js
@@ -321,6 +321,7 @@ const select = {
 
       thisCart.products = [];
       thisCart.getElements(element);
+      thisCart.initActions();
 
       console.log("new Cart", thisCart);
     }
@@ -330,6 +331,15 @@ const select = {
 
       thisCart.dom = {};
       thisCart.dom.wrapper = element;
+      thisCart.dom.toggleTrigger = thisCart.dom.wrapper.querySelector(select.cart.toggleTrigger);
+    }
+
+    initActions(){
+      const thisCart = this;
+
+      thisCart.dom.toggleTrigger.addEventListener("click",  function(){
+        thisCart.dom.wrapper.classList.toggle(classNames.cart.wrapperActive);
+      });
     }
   }
 
